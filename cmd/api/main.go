4fc9bad7a9f2be06f5ee063c6e8395794ffc87be
Diego Alzate <diego35.da@gmail.com>
2024-06-09 10:57:36 +0100

    initial discord message handler

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,15 +2,58 @@ package main
 
 import (
 	"fmt"
-	"jot/internal/server"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+	_ "github.com/joho/godotenv/autoload"
 )
 
 func main() {
+	DISCORD_BOT_TOKEN := os.Getenv("DISCORD_BOT_TOKEN")
 
-	server := server.NewServer()
-
-	err := server.ListenAndServe()
-	if err != nil {
-		panic(fmt.Sprintf("cannot start server: %s", err))
+	if DISCORD_BOT_TOKEN == "" {
+		fmt.Println("DISCORD_BOT_TOKEN is required to run this app")
+		return
 	}
+
+	discord, err := discordgo.New("Bot " + DISCORD_BOT_TOKEN)
+
+	if err != nil {
+		fmt.Println("failed to start discord bot: ", err)
+		return
+	}
+
+	discord.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
+		if m.Author.ID == s.State.User.ID {
+			return
+		}
+
+		if m.Content == "ping" {
+			s.ChannelMessageSend(m.ChannelID, "Pong!")
+		}
+
+		if m.Content == "pong" {
+			s.ChannelMessageSend(m.ChannelID, "Ping!")
+		}
+	})
+
+	// add permissions we require
+	discord.Identify.Intents = discordgo.IntentsGuildMessages
+
+	// open ws connection to discord
+	err = discord.Open()
+	defer discord.Close()
+
+	if err != nil {
+		fmt.Println("failed to start ws connection: ", err)
+		return
+	}
+
+	// wait here until CTRL + C or other term signal is received
+	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-sc
 }
